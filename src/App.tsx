0d8b85e10c11fcffa51b4978536093f17962af0f
Somlói Tamás <tamas.somloi@gmail.com>
2025-04-22 15:38:26 +0200

    első commit

---
+++ src/App.tsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+import { Cost } from './models/cost.model'
+
+function App() {
+
+  const [costs, setCosts] = useState<Cost[]>([])
+  const [error, SetError] = useState('')
+
+  useEffect(()=> {
+    async function fetchData() {
+      const url = "http://localhost:3000/getallcosts"
+      try {
+        const response = await axios.post(
+          url, 
+          //{dateFrom: "2025-04-14"}
+        )
+        const data = response.data
+        if (Array.isArray(data) && data.length > 0) {
+          setCosts(data)
+        } else {
+          SetError('Még nincsenek költségek')
+        }
+      } catch(err) {
+        console.log(err)
+      }
+    }
+    fetchData()
+  }, [])
+
+  useEffect(()=> {
+    console.log(costs)
+  }, [costs])
+
+  return (
+    <>
+      <div>
+        {costs.length > 0 ?
+          <div>
+            {costs.map(cost => <li>{cost.cost_name}</li>)}
+          </div>
+          : error === "" ? 
+            <div>{error}</div> 
+            :
+            <div>Még nincs adatunk</div>
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/models/cost.model.ts
@@ -0,0 +1,8 @@
+export interface Cost {
+    cost_id: number,
+    cost_name: string,
+    cost_amount: number,
+    category_name?: string,
+    category: number,
+    cost_date: string,
+}
